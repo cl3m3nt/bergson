9122a6aaa6abe8f0e941a6572cec0834461f4b49
clementlr <clement.leroux@gmail.com>
2021-02-12 15:41:02 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,277 @@
+import cv2
+import csv
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from PIL import Image
+import tensorflow as tf
+from logzero import logger
+from ephem import readtle, degree
+from exif import Image as exifImage
+import reverse_geocoder as rg
+from pathlib import Path
+from time import sleep
+from datetime import datetime, timedelta
+from picamera import PiCamera
+from keras.models import model_from_json
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+# Data basepath
+dir_path = Path(__file__).parent.resolve()
+data_file = dir_path/'data.csv'
+
+# Ephem ISS location
+name = "ISS (ZARYA)"
+line1 = "1 25544U 98067A   20316.41516162  .00001589  00000+0  36499-4 0  9995"
+line2 = "2 25544  51.6454 339.9628 0001882  94.8340 265.2864 15.49409479254842"
+iss = readtle(name, line1, line2)
+iss.compute()
+
+# Camera Resolution
+rpi_cam = PiCamera()
+rpi_cam.resolution = (648, 486)  # Using AI expected resolution
+
+
+# Camera Data Acquisition
+def capture(camera, image):
+    """
+    Use 'camera' to capture an 'image' file with lat/long EXIF data.
+    """
+    iss.compute()  # Get the lat/long values from ephem
+
+    # convert the latitude and longitude to EXIF-appropriate representations
+    south, exif_latitude = convert(iss.sublat)
+    west, exif_longitude = convert(iss.sublong)
+
+    # set the EXIF tags specifying the current location
+    camera.exif_tags['GPS.GPSLatitude'] = exif_latitude
+    camera.exif_tags['GPS.GPSLatitudeRef'] = "S" if south else "N"
+    camera.exif_tags['GPS.GPSLongitude'] = exif_longitude
+    camera.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
+
+    # capture the image to disk
+    camera.capture(image)
+
+
+# ISS Location Helper functions
+def convert(angle):
+    """
+    Convert an ephem angle (degrees:minutes:seconds) to
+    an EXIF-appropriate representation (rationals)
+    e.g. '51:35:19.7' to '51/1,35/1,197/10'
+    Return a tuple containing a boolean and the converted angle,
+    with the boolean indicating if the angle is negative.
+    """
+    degrees, minutes, seconds = (float(field)
+                                 for field in str(angle).split(":"))
+    exif_angle = f'{abs(degrees):.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10'
+    return degrees < 0, exif_angle
+
+
+# NDVI Helper functions
+def contrast_stretch(im):
+    """
+    Performs a simple contrast stretch of the given image, from 5-100%.
+    """
+    in_min = np.percentile(im, 5)
+    in_max = np.percentile(im, 100)
+
+    out_min = 0.0
+    out_max = 255.0
+
+    out = im - in_min
+    out *= ((out_min - out_max) / (in_min - in_max))
+    out += in_min
+
+    return out
+
+
+def get_ndvi(image_path):
+    """
+    Transform a raw image to ndvi image
+    """
+    image = cv2.imread(image_path)
+    b, g, r = cv2.split(image)
+
+    bottom = (r.astype(float) + b.astype(float))
+    bottom[bottom == 0] = 0.00001  # Make sure we don't divide by zero!
+    ndvi_image = (r.astype(float) - b) / bottom
+    ndvi_image = contrast_stretch(ndvi_image)
+    ndvi_image = ndvi_image.astype(np.uint8)
+    return ndvi_image
+
+
+def get_ndvi_rgb(ndvi_image):
+    """
+    Transform grayscale 3D NDVI image to RGB to be ready for mobilenetv2 prediction
+    """
+    ndvi_rgb = cv2.cvtColor(ndvi_image, cv2.COLOR_GRAY2RGB)
+    return ndvi_rgb
+
+
+def get_img_exif(img_name, iss_var, pred):
+    iss_var.compute()
+    exif_dico = {"Date/Time": datetime.now(), "Location": (iss_var.sublat,
+                                                           iss_var.sublong), "ImgName": img_name, "NO2": pred}
+    return exif_dico
+
+
+# Model Helper functions
+def load_model(model_name):
+    json_file = open(f'{model_name}.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights(f'{model_name}.h5')
+    return loaded_model
+
+
+# Inference Helper functions
+def make_inference(ndvi_image, model):
+    """
+    Make inference using model to get N02 predictions from 1 x NDVI image
+    """
+    ndvi_image_exp = np.expand_dims(ndvi_image, axis=0)
+    ndvi_image_exp = ndvi_image_exp / 255.0
+    prediction = model.predict(ndvi_image_exp)
+    return prediction
+
+
+def load_tflite_model(model_path):
+    """
+    Load TFLite model
+    """
+    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+    return interpreter
+
+
+def make_tflite_inference(ndvi_img_array, model_interpreter):
+    """
+    Make inference using TFLite model to get N02 predictions from 1 x NDVI image
+    """
+    # Get input and output tensors.
+    input_details = model_interpreter.get_input_details()
+    output_details = model_interpreter.get_output_details()
+
+    # Get Input shape
+    input_shape = input_details[0]['shape']
+    input_data = ndvi_img_array.reshape(input_shape)
+
+    model_interpreter.set_tensor(input_details[0]['index'], input_data)
+    model_interpreter.invoke()
+
+    outputs = []
+
+    for tensor in output_details:
+        output_data = model_interpreter.get_tensor(tensor['index'])
+        outputs.append(output_data[0][0])
+
+    prediction = outputs[0]
+
+    return prediction
+
+
+def decode_prediction(prediction):
+    """
+    Decode 3-value float prediction to a string value among "low","medium","high"
+    """
+    no2_level = ["low", "medium", "high"]
+    no2_max = np.argmax(prediction)
+    no2_prediction = no2_level[no2_max]
+    return no2_prediction
+
+
+# Main function
+def main():
+    """
+    Main program process of Bergson Astro Pi team
+    """
+
+    # Experiment Start
+    start_time = datetime.now()
+    logger.info(
+        '################ Bergson Team Experiment Start #################')
+    logger.info(
+        f'Starting Bergson Astro Pi team experiment at {start_time.strftime("%Y-%m-%d %H:%M:%S")}')
+
+    '''
+    # Load simple Conv2D AI Model
+    logger.info("Loading AI Convolutional Model")
+    conv2D_model = load_model("Conv2D_TF114")
+    '''
+
+    # Load TFLite Model
+    logger.info("Loading TFLite Mobilenetv2 Model")
+    mobilenetv2_interpreter = load_tflite_model("./Mobilenetv2_TF114.tflite")
+
+    # Create Log File
+    logger.info(f'Creating Log file at {str(data_file)}')
+    with open(data_file, 'w') as f:
+        writer = csv.writer(f)
+        header = ("Date/time", "Location", "Picture Name", "Predicted NO2")
+        writer.writerow(header)
+
+    # Start Loop over 3 hours
+
+    now_time = datetime.now()
+    i = 0
+    # run a loop for 2 minutes
+    while (now_time < start_time + timedelta(minutes=1)):
+
+        # Take Earth Picture
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+        pic_name = f'bergson_img_{timestamp}.jpg'
+        capture(rpi_cam, str(dir_path/pic_name))
+        logger.info(f'Experiment Pipeline {i} on picture {pic_name}')
+
+        # NDVI Preprocessing
+        ndvi_image = get_ndvi(str(dir_path/pic_name))
+        ndvi_image = np.expand_dims(ndvi_image, axis=2)
+
+        # RGB Prepprocessing for expected shape by Mobilenetv2 - comment below line when using simple Conv2D model
+        ndvi_rgb_image = get_ndvi_rgb(ndvi_image)
+
+        '''
+        # Do Inference with simple Conv2D AI Model
+        prediction = make_inference(ndvi_image,conv2D_model)
+        '''
+        
+        # Do Inference with TFLite Model
+        ndvi_rgb_image = ndvi_rgb_image.astype('float32')
+        prediction = make_tflite_inference(
+            ndvi_rgb_image, mobilenetv2_interpreter)
+
+        # Get Decoded Inference results
+        decoded_prediction = decode_prediction(prediction)
+
+        # Write Prediction as CSV to disk
+        logger.info(
+            f'Logging NO2 prediction \"{decoded_prediction}\" for {pic_name}')
+        exif_data = get_img_exif(pic_name, iss, decoded_prediction)
+        row = (exif_data['Date/Time'], exif_data['Location'],
+               pic_name, exif_data['NO2'])
+        with open(data_file, mode='a') as f:
+            writer = csv.writer(f)
+            writer.writerow(row)
+
+        # update the current time
+        now_time = datetime.now()
+        i = i+1
+
+    # End Loop over 3 hours
+
+    # Experiment End
+    end_time = datetime.now()
+    logger.info(
+        f'Finishing Bergson Astro Pi team experiment at {end_time.strftime("%Y-%m-%d %H:%M:%S")}')
+    experiment_time = end_time - start_time
+    logger.info(f'Bergson Astro Pi team experiment run time {experiment_time}')
+    logger.info('################ Bergson Team Experiment End #################')
+
+
+# Executing main
+main()
---
+++ training.py
@@ -0,0 +1,291 @@
+import cv2
+import matplotlib.pyplot as plt 
+import numpy as np
+import os
+from PIL import Image
+from prettytable import PrettyTable
+import random
+import keras
+from keras.models import Sequential, Model
+from keras.layers import Input,Conv2D, MaxPool2D, GlobalAveragePooling2D, Dense
+from keras.applications import MobileNetV2
+from keras.applications.mobilenet import preprocess_input
+from keras.models import model_from_json
+import logging
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+# Get Data
+base_path = os.getcwd() + '/Dataset'
+image_list = os.listdir('./Dataset')
+
+
+# NDVI helper functions
+def contrast_stretch(im):
+    """
+    Performs a simple contrast stretch of the given image, from 5-100%.
+    """
+    in_min = np.percentile(im, 5)
+    in_max = np.percentile(im, 100)
+
+    out_min = 0.0
+    out_max = 255.0
+
+    out = im - in_min
+    out *= ((out_min - out_max) / (in_min - in_max))
+    out += in_min
+
+    return out
+
+def get_ndvi(image_path):
+    """
+    Transform a raw image to ndvi image
+    """
+    image = cv2.imread(image_path) 
+    b, g, r = cv2.split(image)
+
+    bottom = (r.astype(float) + b.astype(float))
+    bottom[bottom == 0] = 0.00001  # Make sure we don't divide by zero!
+    ndvi_image = (r.astype(float) - b) / bottom
+    ndvi_image = contrast_stretch(ndvi_image)
+    ndvi_image = ndvi_image.astype(np.uint8)
+    return ndvi_image
+
+
+def ndvi_images()->np.array:
+    """ Transform Training RGB picture to NDVI images
+    Returns:
+        ndvi_img_numpy (numpy): a numpy array containing NDVI images as numpy array
+    """
+
+    ndvi_img_list = []
+    for i in range(0,len(image_list)):
+        img_path = os.path.join(base_path,image_list[i])
+        ndvi_img = get_ndvi(img_path)
+        ndvi_img_list.append(ndvi_img)
+    ndvi_img_numpy = np.array(ndvi_img_list)
+    return ndvi_img_numpy
+
+def ndvi_small_image()->list:
+    """ Scale down Raw pictures to fit smaller shape expected by Neural Network
+    Returns:
+        ndvi_img_list (list): the scaled picture list
+    """
+
+    ndvi_img_list = []
+    for i in range(0,len(image_list)):
+        img_path = os.path.join(base_path,image_list[i])
+        ndvi_img = get_ndvi(img_path)
+        ndvi_img = scale_down(ndvi_img)
+        ndvi_img_list.append(ndvi_img)
+    return ndvi_img_list
+
+def ndvi_rgb_image()->np.array:
+    """ Transform a 3D grayscale NDVI image (x,y,1) to 3D RGB image
+    Returns:
+        ndvi_rgb_numpy (np.array): a 3D NDVI image of shape (x,y,3)
+    """
+
+    ndvi_rgb_list = []
+    ndvi_images = ndvi_small_image()
+    for i in range(0,len(image_list)):
+        ndvi_rgb = cv2.cvtColor(ndvi_images[i],cv2.COLOR_GRAY2RGB)
+        ndvi_rgb_list.append(ndvi_rgb)
+    ndvi_rgb_numpy = np.array(ndvi_rgb_list)
+    return ndvi_rgb_numpy
+
+def no2_labels()->np.array:
+    """ Random NO2 labels
+    Returns:
+        label_numpy (np.array): 
+    """
+    label_list = []
+    for i in range(0,82):
+        random_label = random.randint(0,2)
+        label_list.append(random_label)
+    label_numpy = np.array(label_list)
+    return label_numpy
+
+
+def no2_true_labels()->np.array:
+    """ Manually tagged NDVI image with true (evaluated) NO2 label 
+    Returns:
+        label_numpy (np.array): 
+    """
+
+    label_list = [1,1,1,2,0,1,1,1,1,1,2,1,2,1,2,2,1,1,1,1,0,0,2,2,2,2,1,2,1,2,2,2,1,0,1,2,2,0,1,2,2,1,2,0,1,2,1,2,2,1,1,2,2,2,2,2,1,1,1,1,1,2,2,1,0,2,2,1,2,2,2,1,0,0,1,2,2,2,1,0,1,1]
+    label_numpy = np.array(label_list)
+    return label_numpy
+
+
+def scale_down(image:np.array)->np.array:
+    """ scale down raw pictures from Training by 25%
+    Args:
+        image (np.array): a NDVI image 
+    Returns:
+        output (np.array): a scale down NDVI image
+    """
+
+    src = image
+    #percent by which the image is resized
+    scale_percent = 25
+
+    #calculate the 50 percent of original dimensions
+    width = int(src.shape[1] * scale_percent / 100)
+    height = int(src.shape[0] * scale_percent / 100)
+
+    # dsize
+    dsize = (width, height)
+
+    # resize image
+    output = cv2.resize(src, dsize)
+    return output
+
+# Artificial Intelligence Definition
+
+def get_conv2D_model()->object:
+    """ Define a simple conv2D model
+    Returns:
+        model (object): a keras Convolutional Neural Network model
+    """
+
+    model = keras.Sequential([
+    keras.layers.Conv2D(8,(3,3),input_shape=(486,648,1),use_bias=False),
+    keras.layers.MaxPool2D(2,2),
+    keras.layers.Conv2D(16,(3,3),use_bias=False),
+    keras.layers.MaxPool2D(2,2),
+    keras.layers.Conv2D(16,(3,3),use_bias=False),
+    keras.layers.MaxPool2D(2,2),
+    keras.layers.Conv2D(32,(3,3),use_bias=False),
+    keras.layers.MaxPool2D(2,2),
+    keras.layers.Flatten(),
+    keras.layers.Dense(3,activation='softmax')
+])
+    model.compile(
+        optimizer='adam',
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    model.summary()
+    return model
+
+
+def get_mobilenetv2_model():
+    """ Define Mobilenetv2 based model
+    Returns:
+        model (object): a Tranfer Learning model based on Mobilenetv2 Architecture
+    """
+
+    mobilenetv2 = MobileNetV2(include_top=False, weights='imagenet',input_shape=(486,648,3))
+    for layer in mobilenetv2.layers:
+        layer.trainable = False
+    mobilenetv2_preprocess = preprocess_input
+    input_shape = (486,648,3)
+    input = keras.layers.Input(shape=input_shape, name='img_in')
+    x = mobilenetv2_preprocess(input)
+    x = mobilenetv2_preprocess(input)
+    x = mobilenetv2(input)
+    x = keras.layers.GlobalAveragePooling2D()(x)
+    # Classification layer
+    output = keras.layers.Dense(3, activation='softmax', name='dense')(x)
+    # Final model
+    model = Model(inputs=input, outputs=output)
+
+    model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+    )
+
+    model.summary()
+    return model
+
+def save_model(model_name:str,model:object):
+    """ Saving h5 full model
+
+    Args:
+        model_name (str): name of model on disk 
+        model (object): a keras model variable
+    """
+    model.save(model_name + ".h5")
+    print(f"Model saved as {model_name}.h5")
+
+
+
+
+def main():
+
+    # Conv2D Training
+    keras.backend.clear_session()
+    logger.info("Conv2D Training Start")
+    x_train = np.array(ndvi_small_image())
+    x_train = np.expand_dims(x_train,axis=3)
+    x_train = x_train/255.0
+    y_train = no2_true_labels() # train with true label
+    conv2D_model = get_conv2D_model()
+    history_conv2D = conv2D_model.fit(x_train,y_train,epochs=10)
+    save_model("Conv2D_model",conv2D_model)
+
+    # Keras Save model with TF114
+    # serialize model to JSON
+    model_json = conv2D_model.to_json()
+    with open("Conv2D_TF114.json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    conv2D_model.save_weights("Conv2D_TF114.h5")
+    print("Saved model to disk")
+
+    # Keras Load model with TF114
+    # load json and create model
+    json_file = open('Conv2D_TF114.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("Conv2D_TF114.h5")
+    print("Loaded model from disk")
+    print(loaded_model.summary())
+
+
+    # Mobilenetv2 Training
+    logger.info("Mobilenetv2 Training Start")
+    x_train = ndvi_rgb_image()
+    x_train = x_train/255.0
+    y_train = no2_true_labels() # train with true label
+    mobilenetv2_model = get_mobilenetv2_model()
+    history_mobilenetv2 = mobilenetv2_model.fit(x_train,y_train,epochs=20)
+    save_model("Mobilenetv2",mobilenetv2_model)
+
+    # Keras Save Mobilenet for TF114
+    # serialize model to JSON
+    model_json = mobilenetv2_model.to_json()
+    with open("Mobilenetv2_TF114.json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    mobilenetv2_model.save_weights("Mobilenetv2_TF114.h5")
+    print("Saved model to disk")
+
+    # Keras Load model with TF114
+    # load json and create model
+    json_file = open('Mobilenetv2_TF114.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("Mobilenetv2_TF114.h5")
+    print("Loaded model from disk")
+    print(loaded_model.summary())
+
+    # TF 1.14 Convert model to TFLite format for optimization
+    # Convert Model.h5 to TFLite
+    converter = tf.compat.v1.lite.TFLiteConverter.from_keras_model_file("Mobilenetv2.h5")
+    tflite_model = converter.convert()
+    with open('Mobilenetv2_TF114.tflite','wb') as f:
+        f.write(tflite_model)
+
+main()
+
